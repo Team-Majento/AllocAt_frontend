0e3b696140c0c84d1e25c5417be7030caf84532a
thiunuwan <ravinduthiunuwan@gmail.com>
2023-03-17 23:14:42 +0530

    add review-rating-form.component

---
+++ src/app/review-rating/review-rating-form/review-rating-form.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ReviewRatingFormComponent } from './review-rating-form.component';
+
+describe('ReviewRatingFormComponent', () => {
+  let component: ReviewRatingFormComponent;
+  let fixture: ComponentFixture<ReviewRatingFormComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ReviewRatingFormComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ReviewRatingFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/review-rating/review-rating-form/review-rating-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-review-rating-form',
+  templateUrl: './review-rating-form.component.html',
+  styleUrls: ['./review-rating-form.component.scss']
+})
+export class ReviewRatingFormComponent {
+
+}
