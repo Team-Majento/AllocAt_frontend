64a5d59c983e3a5985cb8f06b49f6c551a8bfb53
Nishath <nishathmnha.20@uom.lk>
2023-03-18 15:59:29 +0530

    impliment change password interface validations

--- src/app/user/change-password/change-password.component.ts
+++ src/app/user/change-password/change-password.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, Input, ViewChild} from '@angular/core';
+import {NgForm} from "@angular/forms";
+import {UserLogin} from "../../../models/userLogin";
+import {ChangePssword} from "../../../models/changePssword";
 
 @Component({
   selector: 'app-change-password',
@@ -6,6 +9,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./change-password.component.scss']
 })
 export class ChangePasswordComponent {
+  @ViewChild('InputForm')
+  inputForm!: NgForm;
 
+  @Input()
+  user={} as ChangePssword;
+
+  submit(InputForm:any){
+
+  }
 
 }
---
+++ src/models/changePssword.ts
@@ -0,0 +1,7 @@
+export interface ChangePssword{
+  currentPassword:string;
+  newPassword:string;
+  confirmPassword:string;
+
+
+}
