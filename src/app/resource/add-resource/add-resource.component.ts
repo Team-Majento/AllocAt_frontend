eff3c286d3576cf87420466b4bf7b85386336d98
thiunuwan <ravinduthiunuwan@gmail.com>
2023-03-23 10:16:57 +0530

    update update-resource.component

--- src/app/resource/add-resource/add-resource.component.ts
+++ src/app/resource/add-resource/add-resource.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {Resource} from "../../../models/resource";
+
 
 @Component({
   selector: 'app-add-resource',
--- src/app/resource/update-resource/update-resource.component.ts
+++ src/app/resource/update-resource/update-resource.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ResourceService} from "../../service/resource.service";
+import {ActivatedRoute} from "@angular/router";
+import {Resource} from "../../../models/resource";
 
 @Component({
   selector: 'app-update-resource',
   templateUrl: './update-resource.component.html',
   styleUrls: ['./update-resource.component.scss']
 })
-export class UpdateResourceComponent {
+export class UpdateResourceComponent implements OnInit {
+
+  selectedResource!: Resource;
+
+  resourceId!: string;
+
+
+
+  constructor(private resourceService: ResourceService, private route: ActivatedRoute) {
+    route.params.subscribe(resourceId => {
+      this.resourceId = resourceId["resourceId"];
+      console.log(this.resourceId)
+      if (this.resourceId == null) {
+
+      } else {
+        this.resourceService.getResourceById(this.resourceId).subscribe(
+          (resource) => {
+            console.log(resource);
+            this.selectedResource = <Resource>resource;
+          }
+          , (error) => {
+            console.log(error)
+          });
+      }
+    })
+
+  }
+
+  ngOnInit(): void {
+
+
+  }
+
+
+
+
+
+
+
+
+
+
 
 }
