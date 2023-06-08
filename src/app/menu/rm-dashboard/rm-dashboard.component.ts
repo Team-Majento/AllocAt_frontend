5cc3d5bd807c35ae1f2963e1009418c2d3488bf3
Denuwan <denu1avish@gmail.com>
2023-06-08 23:03:20 +0530

    Fixed issues

--- src/app/menu/rm-dashboard/rm-dashboard.component.ts
+++ src/app/menu/rm-dashboard/rm-dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Chart} from "chart.js";
 
 @Component({
   selector: 'app-rm-dashboard',
@@ -6,5 +7,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./rm-dashboard.component.scss']
 })
 export class RmDashboardComponent {
+  title = 'chartDemo'
+  ngOnInit()
+  {
 
+    new Chart("myChart", {
+      type: 'bar',
+
+      data: {
+
+        labels: ['ABC pvt ltd', 'Thoughtbeat', 'Photobug', 'Fliptune', 'Youobia', 'Snaptags'],
+        datasets: [{
+          label: '# of Votes',
+          data: [6, 8, 4, 6,5, 2],
+          backgroundColor:[
+            '#043e7d',
+            '#046ec4',
+            '#5791b3',
+            '#0e1e2b',
+            '#2e5f78',
+            '#51acc5'
+
+          ],
+          borderWidth: 2
+        }]
+      },
+      options: {
+        scales: {
+          xAxes:[{
+            display:true
+
+          }],
+          yAxes: [{
+            display:true
+            //  beginAtZero: true
+          }]
+        }
+      }
+    });
+  }
 }
