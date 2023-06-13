289e83edca193bb587f9cb19137c5e55a2d9e0d1
Denuwan <denu1avish@gmail.com>
2023-06-14 02:12:21 +0530

    updated dashboard graphs

--- src/app/menu/rm-dashboard/rm-dashboard.component.ts
+++ src/app/menu/rm-dashboard/rm-dashboard.component.ts
@@ -22,6 +22,12 @@ export class RmDashboardComponent implements OnInit{
   totalSubordinates!:number;
   totalRejectedCount!:number;
   totalPendingCount!:number;
+  resourceCountCompany1!:number;
+  resourceCountCompany2!:number;
+  resourceCountCompany3!:number;
+  resourceCountCompany4!:number;
+  resourceCountCompany5!:number;
+  resourceCountCompany6!:number;
 
 
   ngOnInit()
@@ -31,40 +37,14 @@ export class RmDashboardComponent implements OnInit{
     this.getAllUsersCount();
     this.getAllBookingRequests();
     this.getAllSubordinatesCount();
-    new Chart("myChart", {
-      type: 'bar',
+    this.getResourceCountCompany1();
+    this.getResourceCountCompany2();
+    this.getResourceCountCompany3();
+    this.getResourceCountCompany4();
+    this.getResourceCountCompany5();
+    this.getResourceCountCompany6();
 
-      data: {
 
-        labels: ['ABC pvt ltd', 'Thoughtbeat', 'Photobug', 'Fliptune', 'Youobia', 'Snaptags'],
-        datasets: [{
-          label: 'Number of Bookings',
-          data: [6, 8, 4, 6,5, 2],
-          backgroundColor:[
-            '#043e7d',
-            '#046ec4',
-            '#5791b3',
-            '#0e1e2b',
-            '#2e5f78',
-            '#51acc5'
-
-          ],
-          borderWidth: 2
-        }]
-      },
-      options: {
-        scales: {
-          xAxes:[{
-            display:true
-
-          }],
-          yAxes: [{
-            display:true
-            //  beginAtZero: true
-          }]
-        }
-      }
-    });
   }
   constructor(private bookingReqService:BookingRequestService,private location:Location,private service: UserService, private dialogRef: MatDialog, private route: ActivatedRoute, private router: Router) {
     this.userName = localStorage.getItem("userName_") + "";
@@ -150,5 +130,127 @@ export class RmDashboardComponent implements OnInit{
       , error => {
         console.log(error)
       });
+  }
+
+  getResourceCountCompany1(){
+    this.service.getResourceCountCompany1().subscribe(
+      (compileResults) => {
+
+        this.resourceCountCompany1 = compileResults;
+        // @ts-ignore
+        console.log("*"+compileResults);
+      }
+      , error => {
+        console.log(error)
+      }
+    );
+  }
+
+  getResourceCountCompany2(){
+    this.service.getResourceCountCompany2().subscribe(
+      (compileResults) => {
+
+        this.resourceCountCompany2 = compileResults;
+        // @ts-ignore
+        console.log(compileResults);
+        console.log("#")
+      }
+      , error => {
+        console.log(error)
+      }
+    );
+  }
+
+  getResourceCountCompany3(){
+    this.service.getResourceCountCompany3().subscribe(
+      (compileResults) => {
+
+        this.resourceCountCompany3 = compileResults;
+        // @ts-ignore
+      }
+    );
+  }
+
+  getResourceCountCompany4(){
+    this.service.getResourceCountCompany4().subscribe(
+      (compileResults) => {
+
+        this.resourceCountCompany4 = compileResults;
+        // @ts-ignore
+      }
+    );
+  }
+
+  getResourceCountCompany5(){
+    this.service.getResourceCountCompany5().subscribe(
+      (compileResults) => {
+
+        this.resourceCountCompany5 = compileResults;
+        // @ts-ignore
+      }
+    );
+  }
+
+  getResourceCountCompany6(){
+    this.service.getResourceCountCompany6().subscribe(
+      (compileResults) => {
+
+        this.resourceCountCompany6 = compileResults;
+        // @ts-ignore
+        let chartData: number[] = [];
+
+
+        chartData.push(this.resourceCountCompany1);//[10, 20, 30, 40, 50]
+        chartData.push(this.resourceCountCompany2);//[10, 20, 30, 40, 50]
+        chartData.push(this.resourceCountCompany3);//[10, 20, 30, 40, 50]
+        chartData.push(this.resourceCountCompany4);//[10, 20, 30, 40, 50]
+        chartData.push(this.resourceCountCompany5);//[10, 20, 30, 40, 50]
+        chartData.push(this.resourceCountCompany6);//[10, 20, 30, 40, 50]
+        chartData.push(0);//[10, 20, 30, 40, 50]
+
+        // scores.push( 'abc' );	 //data.ts(24,14): error TS2345: Argument of type '"abc"' is not
+        //assignable to parameter of type 'number'.
+        // console.log("######")
+        // console.log(this.resourceCountCompany1)
+        // console.log(this.resourceCountCompany2)
+        // console.log(chartData); // Output: [1, 2, 3]
+        // console.log("#####")
+
+        new Chart("myChart", {
+          type: 'bar',
+
+          data: {
+
+            labels: ['ABC pvt ltd', 'Thoughtbeat', 'Photobug', 'Fliptune', 'Youobia', 'Snaptags'],
+            datasets: [{
+              label: 'Number of Active Resources',
+              data: chartData,
+              backgroundColor:[
+                '#043e7d',
+                '#046ec4',
+                '#5791b3',
+                '#0e1e2b',
+                '#2e5f78',
+                '#51acc5'
+
+              ],
+              borderWidth: 2
+            }]
+          },
+          options: {
+            scales: {
+              xAxes:[{
+                display:true
+
+              }],
+              yAxes: [{
+                display:true
+                //  beginAtZero: true
+              }]
+            }
+          }
+        });
+      }
+    );
   }
 }
