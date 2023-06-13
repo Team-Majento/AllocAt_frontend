7a7dfda60ac408489c66cdecbb8d5bf918aa0bb1
Tharushi Nethmini <tnethmini66@gmail.com>
2023-06-13 16:25:20 -0700

    implemented forget password email sending function

--- src/app/forgot-password/forgot-password.component.ts
+++ src/app/forgot-password/forgot-password.component.ts
@@ -16,28 +16,18 @@ export class ForgotPasswordComponent {
   @ViewChild('forgotPasswordForm')
   inputForm!: NgForm;
   selectedResource!: UserRequest;
-  email:string='';
-
-  userName !: string;
-
-  password !: string;
+  emailTo!:string;
 
   constructor(){}
 
   emailPtn = "^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
 
-
   sendEmail( e: Event) {
-    // Retrieve the username, password, and login page link from the database
-    const username = this.userName;
-    const password = this.password;
-    const loginLink = 'http://localhost:4200/login';
 
-    // Send the email using Email.js
     const templateParams = {
-      username: username,
-      password: password,
-      login_link: loginLink
+      username: this.emailTo,
+      // todo
+      //password: password,
     };
 
     emailjs.send('service_fgfrip8', 'template_6eprlya', templateParams, 'El6uFa2gZTKo5PV1g')
@@ -47,10 +37,9 @@ export class ForgotPasswordComponent {
       }, (error) => {
         console.error('Error sending email:', error);
       });
+
+
   }
-
-
-
 }
 
 
